4367dc0e571e053304878e50d83b01f72084e28d
Shad Mirza <iamshadmirza@gmail.com>
2023-09-16 02:17:15 +0530

    setup project

---
+++ src/constants/images.js
@@ -0,0 +1,23 @@
+export const hat1 = require("../../assets/hat1.png");
+export const jacket1 = require("../../assets/jacket1.png");
+export const jacket2 = require("../../assets/jacket2.png");
+export const jacket4 = require("../../assets/jacket4.png");
+export const shirt1 = require("../../assets/shirt1.png");
+export const shirt2 = require("../../assets/shirt2.png");
+export const shoe1 = require("../../assets/shoe1.png");
+export const sweater3 = require("../../assets/sweater3.png");
+export const sweater5 = require("../../assets/sweater5.png");
+export const searches = require("../../assets/recent-searches.png");
+
+export default {
+  hat1,
+  jacket1,
+  jacket2,
+  jacket4,
+  shirt1,
+  shirt2,
+  shoe1,
+  sweater3,
+  sweater5,
+  searches,
+};
